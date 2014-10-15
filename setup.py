c2a81c3ea8f1d26f09dd36ce357ef24bbbcd30d0
Jeremy Satterfield <jsatt22@gmail.com>
2014-10-15 14:36:46 -0500

    version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -4,21 +4,22 @@ from setuptools import setup
 
 setup(
     name='django-variant',
-    version='0.0.2',
+    version='0.1.0',
     description='Django variant testing framework',
     author='Jeremy Sattefield',
-    author_email='jsatt22@gmail.com',
+    author_email='jsatt@jsatt.com',
     url='https://github.com/jsatt/django-variant',
-    # license='',
     packages=[
         'variant', 'variant.migrations'],
     install_requires=[
         'Django>=1.5', 'six'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Site Management'],
