15874985bbd4a5c27cdce686781bfb0fc22fef67
Ivan Rocha <ivan.cr.neto@gmail.com>
2015-09-15 00:39:23 -0300

    version bump to 0.1.1
    to get latest Buddy changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-variant',
-    version='0.1.0',
+    version='0.1.1',
     description='Django variant testing framework',
     author='Jeremy Sattefield',
     author_email='jsatt@jsatt.com',
